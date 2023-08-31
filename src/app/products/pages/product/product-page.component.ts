34a5977ed954e76d18874c6807e961ca8b53c834
Matías Krabzik <krabzik@gmail.com>
2023-08-31 20:35:18 -0300

    Se implementan todos los LifeCycle Hooks!

--- src/app/products/pages/product/product-page.component.ts
+++ src/app/products/pages/product/product-page.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product-page.component.html',
 })
-export class ProductComponentPage {
+export class ProductComponentPage implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  public isProductVisible: boolean = false;
+
+  toggleVisibleProduct(): void {
+    this.isProductVisible = !this.isProductVisible;
+  }
+  ngDoCheck(): void {
+    console.log(">> ngDoCheck");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    console.log(">> ngOnChanges");
+  }
+
+  ngOnInit(): void {
+    console.log(">> ngOnInit");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log(">> ngAfterContentChecked");
+  }
+
+  ngAfterContentInit(): void {
+    console.log(">> ngAfterContentInit");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(">> ngAfterViewChecked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log(">> ngAfterViewInit");
+  }
 
 }
